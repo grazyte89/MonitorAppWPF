a4829e4ac8bfe3d663f7fed7d4cb9654ea963601
zeroJava <zero_2009@hotmail.co.uk>
2016-03-13 00:46:15 +0000

    commit

---
+++ MonitorAppWPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace MonitorAppWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool State { get; set; }
+        private PerformanceCounter cpuPerformance;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cpuPerformance = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        }
+
+        private void _startButton_Click(object sender, RoutedEventArgs e)
+        {
+            _CPUtextbox.Text = cpuPerformance.NextValue() + "%";
+            while(true)
+            {
+                string i = cpuPerformance.NextValue() + "%";
+                _CPUtextbox.Text = i;
+                _CPUtextbox.Clear();
+            }
+        }
+
+        private void _stopButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
