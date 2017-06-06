6b94ac838585bc7f0b3e951527c4cc0adf748842
zeroJava <zero_2009@hotmail.co.uk>
2017-06-06 22:39:32 +0100

    working on update function

---
+++ PetsEntityLib/DataBaseUpdates/UpdateAnimalClass.cs
@@ -0,0 +1,32 @@
+﻿using PetsEntityLib.DataBaseContext;
+using PetsEntityLib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsEntityLib.DataBaseUpdates
+{
+    public class UpdateAnimalClass : IDBUpdate
+    {
+        private Animal _currentAnimal;
+
+        public UpdateAnimalClass(Animal animal)
+        {
+            _currentAnimal = animal;
+        }
+
+        public void SaveUpdate()
+        {
+            using (PetShopDBContext _context = new PetShopDBContext())
+            {
+                if (_currentAnimal != null)
+                    return;
+                _context.Entry(_currentAnimal).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
