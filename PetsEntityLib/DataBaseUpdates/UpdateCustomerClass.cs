9a666c738386cdd2e482c1dc58318711734af243
zeroJava <zero_2009@hotmail.co.uk>
2017-09-03 04:17:34 +0100

    comments and stuff

--- PetsEntityLib/DataBaseUpdates/UpdateCustomerClass.cs
+++ PetsEntityLib/DataBaseUpdates/UpdateCustomerClass.cs
@@ -30,6 +30,22 @@ namespace PetsEntityLib.DataBaseUpdates
                     this.UpdateMessages(_dbcontext);
 
                 _dbcontext.Entry(_currentCustomer).State = EntityState.Modified;
+                
+                /* Before a parent entity is tagged as medified, make sure all new
+                 * child entity objects in its collection are tagged as Entitysate.Added,
+                 * if the relationship is a one-to-many.
+                 * Because if not, it will throw an error, either saying foreign
+                 * key property does not match or something like duplicate id.
+                 * 
+                 * The reason why it throws an error is because, the child entities
+                 * at that point in time are tagged as Entitysate.Unchanged.
+                 * So when we mark the parent entities as Modified, entity looks
+                 * through its collection and will find new entity objects whose
+                 * foreign key id has not be assigned.
+                 * Or, if the foreign key property is assigned, it will think that
+                 * the parent entity is new, which in this case will throw an error
+                 * because entity knows there is an enitity object with the same id.  
+                 * */
 
                 if (_currentCustomer.Courses != null && _currentCustomer.Courses.Count > 0)
                     this.UpdateCourses(_dbcontext);
@@ -87,10 +103,16 @@ namespace PetsEntityLib.DataBaseUpdates
                 this.AssignEntityStateTags(dbcontext, item, false);
 
             if (dbcontext.Entry(_currentCustomer).State != EntityState.Modified)
+            {
                 dbcontext.Entry(_currentCustomer).State = EntityState.Modified;
-
+            }
             foreach (var item in existingMessages)
                 this.AssignEntityStateTags(dbcontext, item, true);
+
+            /* When tagging child entities in a collection as Entitystate.Modified,
+             * make sure you tag the parent entity as Modified, or else it will throw
+             * am error.
+             * */
 
             _currentCustomer.Messages = detatchedCollection;
         }
