5c5ba5be0f8a1f80a6de65e9a41022abe64829e7
zeroJava <zero_2009@hotmail.co.uk>
2017-10-04 22:25:48 +0100

    qwerty

--- MonitorAppMVVM/DbControlsVM/AnimalSoldVm/AnimalSoldViewModel.cs
+++ MonitorAppMVVM/DbControlsVM/AnimalSoldVm/AnimalSoldViewModel.cs
@@ -1,12 +1,119 @@
-﻿using System;
+﻿using PetsEntityLib.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MonitorAppMVVM.DbControlsVM.AnimalSoldVm
 {
-    public class AnimalSoldViewModel
+    public class AnimalSoldViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ExistingOrNewModel { get; set; }
+
+        private IList<AnimalSold> _animalSoldList;
+        public IList<AnimalSold> AnimalSoldList
+        {
+            get
+            {
+                return _animalSoldList;
+            }
+            set
+            {
+                _animalSoldList = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("AnimalSoldList"));
+            }
+        }
+
+        private bool _animalSoldListAccessEnabled;
+        public bool AnimalSoldListAccessEnabled
+        {
+            get
+            {
+                return _animalSoldListAccessEnabled;
+            }
+            set
+            {
+                _animalSoldListAccessEnabled = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("AnimalSoldListAccessEnabled"));
+            }
+        }
+
+        private AnimalSold _currentAnimalSold;
+        public AnimalSold CurrentAnimalSold
+        {
+            get
+            {
+                return _currentAnimalSold;
+            }
+            set
+            {
+                _currentAnimalSold = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentAnimalSold"));
+            }
+        }
+
+        private AnimalSold _selectedAnimalSold;
+        public AnimalSold SelectedAnimalSold
+        {
+            get
+            {
+                return _selectedAnimalSold;
+            }
+            set
+            {
+                _selectedAnimalSold = value;
+                CurrentAnimalSold = _selectedAnimalSold;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedAnimalSold"));
+            }
+        }
+
+        private EditAnimalSoldCommand _editAnialSoldCommand;
+        public ICommand EditAnimalSoldCommand
+        {
+            get
+            {
+                return _editAnialSoldCommand;
+            }
+        }
+
+        private NewAnimalSoldCommand _newAnimalSoldCommand;
+        public ICommand NewAnimaldSoldCommand
+        {
+            get
+            {
+                return _newAnimalSoldCommand;
+            }
+        }
+
+        private RetrieveAnimalSoldCommand _retrieveAnimalSoldCommand;
+        public ICommand RetrieveAnimalSoldCommand
+        {
+            get
+            {
+                return _retrieveAnimalSoldCommand;
+            }
+        }
+
+        private SaveAnimalSoldCommand _saveAnimalSoldCommand;
+        public ICommand SaveAnimalSoldCommand
+        {
+            get
+            {
+                return _saveAnimalSoldCommand;
+            }
+        }
+
+        public AnimalSoldViewModel()
+        {
+            _editAnialSoldCommand = new EditAnimalSoldCommand(this);
+            _newAnimalSoldCommand = new NewAnimalSoldCommand();
+            _retrieveAnimalSoldCommand = new RetrieveAnimalSoldCommand();
+            _saveAnimalSoldCommand = new SaveAnimalSoldCommand();
+        }
     }
 }
---
+++ MonitorAppMVVM/DbControlsVM/AnimalSoldVm/EditAnimalSoldCommand.cs
@@ -0,0 +1,32 @@
+﻿using MonitorAppMVVM.DbControlsVM.AnimalSoldVm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MonitorAppMVVM.DbControlsVM.AnimalSoldVm
+{
+    public class EditAnimalSoldCommand : ICommand
+    {
+        private AnimalSoldViewModel _animalSoldViewModel;
+
+        public event EventHandler CanExecuteChanged;
+
+        public EditAnimalSoldCommand(AnimalSoldViewModel animalSoldViewModel)
+        {
+            _animalSoldViewModel = animalSoldViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
