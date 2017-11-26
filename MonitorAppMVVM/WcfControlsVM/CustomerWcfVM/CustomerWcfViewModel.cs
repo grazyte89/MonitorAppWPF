73164cbfbf24ca483c5d1a5c6e7f3e4bb00d25b1
zeroJava <zero_2009@hotmail.co.uk>
2017-11-26 22:56:44 +0000

    working on integrating wcf with wpf project

---
+++ MonitorAppMVVM/WcfControlsVM/CustomerWcfVM/CustomerWcfViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorAppMVVM.WcfControlsVM.CustomerWcfVM
+{
+    public class CustomerWcfViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int iiii;
+    }
+}
