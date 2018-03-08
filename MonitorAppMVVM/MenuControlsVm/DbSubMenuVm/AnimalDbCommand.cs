ea0b197a2b97d150a0e9fd1158a7ae6eb5ab3607
zeroJava <zero_2009@hotmail.co.uk>
2018-03-08 23:30:45 +0000

    Revert "deleted something"
    This reverts commit 5101b6ce19423298b74e3284ea26dfecc8682bdc.

---
+++ MonitorAppMVVM/MenuControlsVm/DbSubMenuVm/AnimalDbCommand.cs
@@ -0,0 +1,33 @@
+﻿using MonitorAppMVVM.DbControlsVM.AnimalVm;
+using MonitorAppMVVM.VmSharedGeneric;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MonitorAppMVVM.MenuControlsVM.DbSubMenuVM
+{
+    public class AnimalDbCommand : ICommand
+    {
+        private DBSubMenuControlViewModel _dbSubMenuViewModel;
+
+        public event EventHandler CanExecuteChanged;
+
+        public AnimalDbCommand(DBSubMenuControlViewModel dbSubMenuViewModel)
+        {
+            _dbSubMenuViewModel = dbSubMenuViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _dbSubMenuViewModel.DbControlViewModel = new AnimalViewModel();
+        }
+    }
+}
---
+++ MonitorAppMVVM/MenuControlsVm/DbSubMenuVm/DBSubMenuControlViewModel.cs
@@ -0,0 +1,78 @@
+﻿using MonitorAppMVVM.VmSharedGeneric;
+using MonitorAppMVVM.MainWindowVM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MonitorAppMVVM.DbControlsVM.AnimalVm;
+using MonitorAppMVVM.ContainerVM;
+
+namespace MonitorAppMVVM.MenuControlsVM.DbSubMenuVM
+{
+    public class DBSubMenuControlViewModel : IGenericBaseViewModel, INotifyPropertyChanged
+    {
+        public string CurrentViewModelName { get { return "DBSubMenuControlViewModel"; } }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private IGenericBaseViewModel _dbControlViewModel;
+        public IGenericBaseViewModel DbControlViewModel
+        {
+            get
+            {
+                return _dbControlViewModel;
+            }
+            set
+            {
+                _dbControlViewModel = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DbControlViewModel"));
+            }
+        }
+
+        private AnimalDbCommand _animalDbCommand;
+        public ICommand AnimalDbCommand
+        {
+            get
+            {
+                return _animalDbCommand;
+            }
+        }
+
+        private AnimalSoldDbCommand _animalSoldDbCommand;
+        public ICommand AnimalSoldDbCommand
+        {
+            get
+            {
+                return _animalSoldDbCommand;
+            }
+        }
+
+        private CustomerDbCommand _customerDbCommand;
+        public ICommand CustomerDbCommand
+        {
+            get
+            {
+                return _customerDbCommand;
+            }
+        }
+
+        private StockDbCommand _stockDbCommand;
+        public ICommand StockDbCommand
+        {
+            get
+            {
+                return _stockDbCommand;
+            }
+        }
+
+        public DBSubMenuControlViewModel()
+        {
+            _animalDbCommand = new AnimalDbCommand(this);
+            _animalSoldDbCommand = new AnimalSoldDbCommand(this);
+            _customerDbCommand = new CustomerDbCommand(this);
+            _stockDbCommand = new StockDbCommand(this);
+        }
+    }
+}
